3dc8b0cfdb46713dbc9383f9dfe230bc97b25653
Colin Malcolm <colinmalcolm8@gmail.com>
2021-10-19 22:07:05 -0400

    working on project cards

--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -15,34 +15,67 @@ class Work extends Component {
        
         <h1 className="projectheading">Projects</h1>
 
+        <div className="work-section">
+
         <div className="card">
           <div className="image-data">
             <div className="background-image"></div>
 
             <div className="project-details">
               
+              <p class="left-title">What Did I Use</p>
               
-              {/* class="author" below */}
-              <a href="https://github.com/malcolmc10/Pandemic" class="github">
-                <AiFillGithub class="icon1" /></a>
-              
-              <a href="https://covid-country-e9bff.web.app/" class="website">
-              <CgBrowser class="icon2" /></a>
+             
             
             
             </div>
           
           
           </div>
-      
-        
+
+          <div className="post-data"></div>
+          <h1 class="project-title">COVID Country</h1>
+          <h2 class="project-subtitle">A Project With A Purpose</h2>
+          <p className="project-description">Using the disease.sh API, Leaflet, and Material UI I created an interactive tool that allows the user to track the numbers of cases, recoveries, and deaths in countries all over the world using a line graph, bubbles, and charts.</p>
+
+          <div className="cta">
+              <a href="https://github.com/malcolmc10/Pandemic" rel="noreferrer" target="_blank" class="github">
+                <AiFillGithub class="icon1" /></a>
+              
+              <a href="https://covid-country-e9bff.web.app/" rel="noreferrer" target="_blank" class="website">
+              <CgBrowser class="icon2" /></a>
+
+          </div>
+
+
         </div>
         
 
-
+        </div>
       </div>
     );
   }
 }
 
 export default Work;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ {/* class="author" below */}
+              {/* <a href="https://github.com/malcolmc10/Pandemic" rel="noreferrer" target="_blank" class="github">
+                <AiFillGithub class="icon1" /></a>
+              
+              <a href="https://covid-country-e9bff.web.app/" rel="noreferrer" target="_blank" class="website">
+              <CgBrowser class="icon2" /></a> */}
