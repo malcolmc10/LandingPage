9fca272197816cab48073217da3953ba0b4f486e
Colin Malcolm <colinmalcolm8@gmail.com>
2021-10-19 20:37:51 -0400

    adding work section details

--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import './Work.css'; 
+import './Work.css';
+import { AiFillGithub } from "react-icons/ai"
+import {CgBrowser} from "react-icons/cg"
+// import Box from "./components/Box.png"
+// import Clima from "./components/Clima.png"
+// import Covid from "./components/Covid.png"
+// import Harry from "./components/Harry.png"
 
 class Work extends Component {
   render() {
@@ -8,6 +14,29 @@ class Work extends Component {
         {/* <span class="dot"></span> */}
        
         <h1 className="projectheading">Projects</h1>
+
+        <div className="card">
+          <div className="image-data">
+            <div className="background-image"></div>
+
+            <div className="project-details">
+              
+              
+              {/* class="author" below */}
+              <a href="https://github.com/malcolmc10/Pandemic" class="github">
+                <AiFillGithub class="icon1" /></a>
+              
+              <a href="https://covid-country-e9bff.web.app/" class="website">
+              <CgBrowser class="icon2" /></a>
+            
+            
+            </div>
+          
+          
+          </div>
+      
+        
+        </div>
         
 
 
